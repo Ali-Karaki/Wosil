cdf0679f60a4e2f4a28f30c3d24ba40af6aa3c24
Johnny Jabbour <jgj09@mail.aub.edu>
2022-02-25 23:06:22 +0200

    drivers screen design

--- components/Order.js
+++ components/Order.js
@@ -1,49 +1,64 @@
 import React from "react";
-import { StyleSheet,View,Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 const Order = ({ order }) => {
   return (
-    
     <View
-    style={{
-      backgroundColor: "#C4C4C4",
-      position: "relative",
-      width: "85%",
-      height: "100%",
-      left: 30,
-      borderColor: "#5E40BC",
-      //marginTop:"-40%",
-      borderWidth: 1.5,
-      borderRadius: 30,
-      shadowColor: "#000000",
-      shadowOffset: {
-        width: 40,
-        height: 40,
-      },
-      opacity: 0.7,
-      elevation: 2,
+      style={{
+        backgroundColor: "#C4C4C4",
+        position: "relative",
+        width: "85%",
+        height: "100%",
+        left: 30,
+        borderColor: "#5E40BC",
+        //marginTop:"-40%",
+        borderWidth: 1.5,
+        borderRadius: 30,
+        shadowColor: "#000000",
+        shadowOffset: {
+          width: 40,
+          height: 40,
+        },
+        opacity: 0.7,
+        elevation: 2,
 
-      justifyContent: "center",
-    }}
+        justifyContent: "center",
+      }}
     >
-     <Text style={styles.text}> Pick up Location : {order.pickupLocation}</Text>
-      <Text style={styles.text}> Dropoff Building : {order.dropoffLocation.building}</Text>
-      <Text style={styles.text}> Dropoff City : {order.dropoffLocation.city}</Text>
-      <Text style={styles.text}> Dropoff Floor: {order.dropoffLocation.floor}</Text>
-      <Text style={styles.text}> Dropoff Street: {order.dropoffLocation.street}</Text>
-      <Text style={styles.text}> Customer phone number: {order.phoneNumberCustomer} </Text>
-  
+      <Text style={styles.text}>
+        {" "}
+        Pick up Location : {order.pickupLocation}
+      </Text>
+      <Text style={styles.text}>
+        {" "}
+        Dropoff City : {order.dropoffLocation.city}
+      </Text>
+      <Text style={styles.text}>
+        {" "}
+        Dropoff Street: {order.dropoffLocation.street}
+      </Text>
+      <Text style={styles.text}>
+        {" "}
+        Dropoff Building : {order.dropoffLocation.building}
+      </Text>
+      <Text style={styles.text}>
+        {" "}
+        Dropoff Floor: {order.dropoffLocation.floor}
+      </Text>
+      <Text style={styles.text}>
+        {" "}
+        Customer phone number: {order.phoneNumberCustomer}{" "}
+      </Text>
       <Text style={styles.text}> Price: {order.price} </Text>
       <Text style={styles.text}> DeliverCharge: {order.deliveryCharge} </Text>
     </View>
   );
 };
-
-export default Order;
-
 const styles = StyleSheet.create({
   text: {
-    fontSize: 12, color: "#000",marginLeft:"5%",
-
+    fontSize: 12,
+    color: "#000",
+    marginLeft: "5%",
   },
 });
+export default Order;
